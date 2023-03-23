0060e77cbb23b460100fa222fe9c17c95e9bdf2f
David Nogales <davidnogales1214@gmail.com>
2023-03-23 21:17:13 +0100

    added Frontend pages

--- app/deep-ear-client/astro.config.mjs
+++ app/deep-ear-client/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
       usePolling: true
     }
   },
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
--- app/deep-ear-server/main.py
+++ app/deep-ear-server/main.py
@@ -1,22 +1,53 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+abc_song = """X:1
+T:Speed the Plough
+M:4/4
+C:Trad.
+K:G
+|:GABc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|
+  GABc dedB|dedB dedB|c2ec B2dB|A2F2 G4:|
+|:g2gf gdBd|g2f2 e2d2|c2ec B2dB|c2A2 A2df|
+  g2gf g2Bd|g2f2 e2d2|c2ec B2dB|A2F2 G4:|
+    """
 
-class User(BaseModel):
-    first_name: str
-    last_name: str = None
-    age: int
+class Exercise(BaseModel):
+    header: str
+    body: str = None
+    #age: int
 
+example_exercises = {
+   0: Exercise(header="Exercise",body=abc_song)
+}
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/exercises/")
+async def create_exercise(exercise: Exercise):
+    exercise.body = example_exercises[0].body
+    return exercise
 
 @app.get("/data/")
 async def get_data():
  return {"data":"This is important data!"}
 
 
-@app.get("/greeting/")
+@app.get("/")
 async def root():
- return {"greeting":"Hello world"}
+ return {"greeting":"Hello world",
+         "data":example_exercises
+         }
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
