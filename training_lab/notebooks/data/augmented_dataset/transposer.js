d562a8a8158f9c0820c5c7a316fad2e080e6c36b
David Nogales <davidnogales1214@gmail.com>
2023-04-10 07:44:56 +0200

    transposer finished and augmented data corrected

--- training_lab/notebooks/data/augmented_dataset/transposer.js
+++ training_lab/notebooks/data/augmented_dataset/transposer.js
@@ -1,40 +1,28 @@
 console.log('starting script...')
-const jsdom = require('jsdom');
 
-const { JSDOM } = jsdom;
-
-const dom = new JSDOM(`<!DOCTYPE html><body><div id="paper"></div></body>`);
-const { window } = dom;
-
-global.document = window.document;
-global.window = window;
-global.navigator = {
-  userAgent: 'node.js',
-};
 
 const fs = require('fs');
-const abcjs = require('abcjs');
+const {transposeABC} = require('abc-notation-transposition');
 
-const keys = [1, 2, 3,4, 5, 6, -1, -2, -3, -4, -5];
+const steps = [1, 2, 3,4, 5, 6, -1, -2, -3, -4, -5];
 
 console.log('starting reading file...')
 song = ''
 
-var visualObj = abcjs.renderAbc("paper", song);
-
 fs.readFile('dataset_0.abc', 'utf-8', (err, data) => {
   if (err) throw err;
   const songs = data.split('\n\n');
-  keys.forEach(key => {
-    const transposedSongs = songs.map(song => abcjs.strTranspose(song ,visualObj, key));
+  steps.forEach(step => {
+    
+    const transposedSongs = songs.map(song => output = transposeABC(song ,step));
     const transposedData = transposedSongs.join('\n\n');
-    const filename = key < 0 ? `dataset_min${Math.abs(key)}.txt` : `dataset_${key}.txt`;
+    const filename = step < 0 ? `dataset_min${Math.abs(step)}.abc` : `dataset_${step}.abc`;
     fs.writeFile(filename, transposedData, err => {
       if (err) throw err;
       console.log(`File ${filename} is written successfully.`);
     });
+    console.log('Transposing finished succesfully')
   });
-  console.log('Transposing finished succesfully')
 });
 
 
