a0b7ee9260a2ecfe2d3cd6e98138d1da81cd213d
David Nogales <davidnogales1214@gmail.com>
2023-05-04 02:54:50 +0200

    fixed player on tune change

--- app/deep-ear-client/src/scripts/exercisePlayer.js
+++ app/deep-ear-client/src/scripts/exercisePlayer.js
@@ -4,23 +4,42 @@ var abcString = "X:1\nT:Speed the Plough\nM:4/4\nC:Trad.\nK:G\ng2gf g2Bd|g2f2 e2
 var visualOptions = { responsive: 'resize' };
 var synthControl = new abcjs.synth.SynthController();
 
+
+export function setNewEditor(editAreaID,canvasID,warningsID){
+    var editor = new abcjs.Editor(editAreaID, {
+        canvas_id: canvasID,
+        warnings_id: warningsID,
+        onchange: function () {
+            // your code here
+            console.log("changed editAre inside the options of the editor created")
+            const notationInput = document.querySelector('#notation-input');
+            synthControl = new abcjs.synth.SynthController();
+            renderAbcSong(canvasID,notationInput.value)
+        },
+        abcjsParams: {}
+    });
+}
+
+
+
 export function renderAbcSong(divID,abc_song){
-    console.log(divID)
+    //console.log(divID)
     let visualObj = abcjs.renderAbc(divID, abc_song, visualOptions);
-    console.log("rendered song")
-    console.log(visualObj)
+    //console.log("rendered song")
+    //console.log(visualObj)
     activate_sound(visualObj)
 }
 
 function activate_sound(tunes) {    
-    console.log("Activate audio clicked!")
+    //console.log("Activate audio clicked!")
+    synthControl = new abcjs.synth.SynthController();
     if (abcjs.synth.supportsAudio()) {
         var audioParams = { chordsOff: true };
-        console.log("Supports audio!")
+        //console.log("Supports audio!")
         var createSynth = new abcjs.synth.CreateSynth();
         createSynth.init({ visualObj: tunes[0]}).then(function () {
             synthControl.setTune(tunes[0], false, audioParams).then(function () {
-                console.log("Audio successfully loaded.")
+                //console.log("Audio successfully loaded.")
                 
             }).catch(function (error) {
                 console.warn("Audio problem:", error);
