3f812a76b82c3d2177877946c55c3283873b6e5b
David Nogales <david.nogales.dev@gmail.com>
2025-03-12 00:00:31 +0100

    Add astro config

--- app/deep-ear-client/astro.config.mjs
+++ app/deep-ear-client/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://DavidNogales.github.io',
+  base: '/DeepEarTraining',
   vite: {
     plugins: [tailwindcss()],
   },
